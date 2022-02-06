8a6e18ea161720cfe0ed4caf8b58fac6d89873a2
Marc Pérez de Tudela <mperezdetudelaaznar@gmail.com>
2022-02-06 02:24:07 +0100

    style: add accesibility style to SocialMediaIcon

--- src/components/SocialMediaIcon.tsx
+++ src/components/SocialMediaIcon.tsx
@@ -11,7 +11,7 @@ const SocialMediaIcon = ({ link, children, mediaName }: Props) => {
       href={link}
       target="_blank"
       rel="dns-prefetch noopener noreferrer"
-      className="transition-all fill-secondary duration-700 hover:translate-y-1 hover:scale-125 bg-transparent border border-solid border-secondary shadow-xl font-semibold px-4 py-4 rounded-lg w-min"
+      className="transition-all duration-700 hover:translate-y-1 hover:scale-125 bg-transparent border border-solid border-secondary shadow-xl font-semibold px-4 py-4 rounded-lg w-min"
     >
       {children}
     </a>
