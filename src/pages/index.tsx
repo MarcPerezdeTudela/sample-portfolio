e4f7f85810fadf1d92f5f1d52c42564cab232e14
Marc Pérez de Tudela <mperezdetudelaaznar@gmail.com>
2022-02-09 06:23:30 +0100

    refactor: move Layout component from app to index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,33 +6,39 @@ import ProjectsSection from '@/components/sections/ProjectsSection'
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import { Fade } from 'react-awesome-reveal'
+import Layout from '@/components/layout/Layout'
 
 const Home: NextPage = () => {
   return (
-    <div className="grid grid-cols-1 gap-y-20 min-h-screen">
-      <Head>
-        <title>Sample Portfolio</title>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <meta
-          name="description"
-          content="Sample portfolio to practise Nextjs and TailwindCSS"
-        ></meta>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <HeroSection />
-      <Fade direction="up" triggerOnce={true} fraction={0.25}>
-        <AboutSection />
-      </Fade>
-      <Fade direction="up" triggerOnce={true} fraction={0.25}>
-        <JobsSection />
-      </Fade>
-      <Fade direction="up" triggerOnce={true} fraction={0.1}>
-        <ProjectsSection />
-      </Fade>
-      <Fade direction="up" triggerOnce={true} fraction={0.25}>
-        <ContactSection />
-      </Fade>
-    </div>
+    <Layout>
+      <div className="grid grid-cols-1 gap-y-20 min-h-screen">
+        <Head>
+          <title>Sample Portfolio</title>
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, width=device-width"
+          />
+          <meta
+            name="description"
+            content="Sample portfolio to practise Nextjs and TailwindCSS"
+          ></meta>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <HeroSection />
+        <Fade direction="up" triggerOnce={true} fraction={0.25}>
+          <AboutSection />
+        </Fade>
+        <Fade direction="up" triggerOnce={true} fraction={0.25}>
+          <JobsSection />
+        </Fade>
+        <Fade direction="up" triggerOnce={true} fraction={0.1}>
+          <ProjectsSection />
+        </Fade>
+        <Fade direction="up" triggerOnce={true} fraction={0.25}>
+          <ContactSection />
+        </Fade>
+      </div>
+    </Layout>
   )
 }
 
